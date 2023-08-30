667c0bc831c5f5b2bab589c7f0d3826e2970eea4
Jusouza182 <juliana_s_souza@estudante.sesisenai.org.br>
2023-08-29 22:17:26 -0300

    feat: criação do projeto com Router
    -Rota de páginas configuradas

---
+++ fit_manage_tech/src/router/index.js
@@ -0,0 +1,56 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import AlunosNovo from "../views/Alunos/Novo/AlunosNovo.vue"
+import AlunosGerenciamento from "../views/Alunos/Gerenciamento/AlunosGerenciamento.vue"
+import Cadastro from "../views/Cadastro/CadastroView.vue"
+import Dashboard from "../views/Dashboard/DashboardView.vue"
+import Exercicios from "../views/Exercicios/ExerciciosView.vue"
+import Login from '../views/Login/LoginView.vue'
+import TreinoNovo from "../views/Treino/Novo/TreinoNovo.vue"
+import TreinoVizualizacao from "../views/Treino/Vizualizacão/TreinoVizualizacao.vue"
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/cadastro',
+      name: 'cadastro',
+      component: Cadastro
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/alunos/novo',
+      name: 'novo aluno',
+      component: AlunosNovo
+    },
+    {
+      path: '/alunos',
+      name: 'alunos',
+      component: AlunosGerenciamento
+    },
+    {
+      path: '/exercicios',
+      name: 'exercicios',
+      component: Exercicios
+    },
+    {
+      path: '/treinos/novo',
+      name: 'novo treino',
+      component: TreinoNovo
+    },
+    {
+      path: '/treino',
+      name: 'treino',
+      component: TreinoVizualizacao
+    }
+  ]
+})
+
+export default router
