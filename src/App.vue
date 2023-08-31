e0b7a61834ab03b299de524a0c495bfd814b9980
Jusouza182 <juliana_s_souza@estudante.sesisenai.org.br>
2023-08-30 22:03:44 -0300

    feat: cabecalho e rodape

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
 <template>
+  <Cabecalho v-if="renderizarMenu"></Cabecalho>
   <router-view></router-view>
+  <Rodape v-if="renderizarMenu"></Rodape>
 </template>
+<script>
+import Rodape from "./components/Rodape/RodapeView.vue"
+import Cabecalho from "./components/Cabecalho/CabelhoView.vue"
+export default {
+  components: {
+    Rodape,
+    Cabecalho
+  },
+  computed: {
+    renderizarMenu() {
+      return this.$route.path !== '/' && this.$route.path !== '/cadastro'
+    }
+  }
+}
+</script>
