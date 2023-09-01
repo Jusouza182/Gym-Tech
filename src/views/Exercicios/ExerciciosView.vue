243d3dc5e8a80234a2abe8b10f6b8cc2d7ed6545
Jusouza182 <juliana_s_souza@estudante.sesisenai.org.br>
2023-08-31 22:51:25 -0300

    update tela exercicio css

--- src/views/Exercicios/ExerciciosView.vue
+++ src/views/Exercicios/ExerciciosView.vue
@@ -7,7 +7,7 @@
     <v-form class="d-flex" style="width: 60%; margin: 2% auto;" @submit.prevent>
         <v-text-field variant="outlined" v-model="exercicio" label="Novo Exercício"
             placeholder="Digite um novo exercício"></v-text-field>
-        <v-btn type="submit" variant="outlined" class="bg-blue" height="50px" style="margin-left: 2%; margin-top: 0.5%;">Cadastrar</v-btn>
+        <v-btn type="submit"  class="bg-blue" height="40px" style="margin-left: 2%; margin-top: 0.8%;">Cadastrar</v-btn>
     </v-form>
     <v-table fixed-header style="width: 60%; margin: 0 auto;" height="300px">
         <thead>
