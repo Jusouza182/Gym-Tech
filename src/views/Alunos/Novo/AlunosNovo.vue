5f9948f844511b77b6f74487eb8af2c66b2585eb
Jusouza182 <juliana_s_souza@estudante.sesisenai.org.br>
2023-09-03 17:28:34 -0300

    feat: requisição e axios post

--- src/views/Alunos/Novo/AlunosNovo.vue
+++ src/views/Alunos/Novo/AlunosNovo.vue
@@ -6,48 +6,58 @@
     <v-divider :thickness="2" class="border-opacity-70" width="80%" style="margin: 0 auto;"></v-divider>
 
     <v-card class="elevation-0">
-        <v-form @submit.prevent style="width: 80%; margin: 0 auto;">
+        <v-form @submit.prevent="cadastrar" style="width: 80%; margin: 0 auto;" ref="form">
+
             <div style="width: 80%; margin: 02% auto;">
-                <div class="d-flex" style="gap:10px">
+
+                <div class="d-flex" style="gap:10px; margin-bottom: 12px;">
                     <v-text-field variant="outlined" v-model="nome" label="Nome completo"
-                        placeholder="Digite o nome completo" type="text" style="width: 10%"></v-text-field>
+                        placeholder="Digite o nome completo" type="text" style="width: 10%"
+                        :rules="[rules.required]"></v-text-field>
 
                     <v-text-field variant="outlined" v-model="email" label="E-mail" placeholder="Digite o e-mail"
-                        type="email" style="width: 10%"></v-text-field>
+                        type="email" style="width: 10%" :rules="[rules.email, rules.required]"></v-text-field>
                 </div>
 
-                <div class="d-flex" style="gap:10px">
+                <div class="d-flex" style="gap:10px; margin-bottom: 12px;">
                     <v-text-field variant="outlined" v-model="contato" label="Contato"
-                        placeholder="Digite o telefone para contato" type="number" style="width: 10%"></v-text-field>
+                        placeholder="Digite o telefone para contato" type="number" style="width: 10%"
+                        :rules="[rules.required]"></v-text-field>
 
                     <v-text-field variant="outlined" v-model="dataNasimento" label="Data de nascimento"
-                        placeholder="Digite a data de nascimento" type="date" style="width: 10%"></v-text-field>
+                        placeholder="Digite a data de nascimento" type="date" style="width: 10%"
+                        :rules="[rules.required]"></v-text-field>
                 </div>
 
-                <div class="d-flex" style="gap:10px">
+                <div class="d-flex" style="gap:10px; margin-bottom: 12px;">
 
                     <v-text-field variant="outlined" v-model="CEP" label="CEP" placeholder="Digite o CEP" type="number"
-                        style="width: 10%"></v-text-field>
+                        style="width: 4%" :rules="[rules.required]"></v-text-field>
+
+                    <v-text-field variant="outlined" v-model="numero" label="Número" placeholder="Digite o número" type="number"
+                        style="width: 2%" :rules="[rules.required]"></v-text-field>
 
                     <v-text-field variant="outlined" v-model="logradouro" label="Logradouro"
-                        placeholder="Digite o logradouro" type="text" style="width: 10%"></v-text-field>
+                        placeholder="Digite o logradouro" type="text" style="width: 35%"
+                        :rules="[rules.required]"></v-text-field>
                 </div>
 
-                <div class="d-flex" style="gap:10px">
-                    <v-select variant="outlined" v-model="estado" label="Estado"
-                        placeholder="Selecione o estado"></v-select>
+                <div class="d-flex" style="gap:10px; margin-bottom: 12px;">
+                    <v-text-field variant="outlined" v-model="estado" label="Estado" placeholder="Digite o estado"
+                        type="text" :rules="[rules.required]"></v-text-field>
+
                     <v-text-field variant="outlined" v-model="bairro" label="Bairro" placeholder="Digite o bairro"
-                        type="text"></v-text-field>
+                        type="text" :rules="[rules.required]"></v-text-field>
 
                     <v-text-field variant="outlined" v-model="cidade" label="Cidade" placeholder="Digite a cidade"
-                        type="text"></v-text-field>
+                        type="text" :rules="[rules.required]"></v-text-field>
 
                     <v-text-field variant="outlined" v-model="complemento" label="Complemento"
                         placeholder="Digite o complemento" type="text"></v-text-field>
 
                 </div>
                 <v-card-actions class="d-flex justify-end">
-                    <v-btn class="bg-blue" width="150" height="40">Cadastrar</v-btn>
+                    <v-btn class="bg-blue" width="150" height="40" type="submit">Cadastrar</v-btn>
                 </v-card-actions>
             </div>
 
@@ -55,20 +65,75 @@
     </v-card>
 </template>
 <script>
-export default{
-    data(){
-        return{
-            nome:'',
-            email:'',
-            contato:'',
-            dataNasimento:'',
-            CEP:'',
-            logradouro:'',
-            estado:'',
-            bairro:'',
-            cidade:'',
-            complemento:''
+import axios from "axios"
+export default {
+    data() {
+        return {
+            nome: '',
+            email: '',
+            contato: '',
+            dataNasimento: '',
+            CEP: '',
+            logradouro: '',
+            estado: '',
+            bairro: '',
+            cidade: '',
+            complemento: '',
+            numero: '',
+            rules: {
+                required: value => !!value || '*Campo obrigatório',
+                email: value => {
+                    const confirmaEmail =
+                        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                    return confirmaEmail.test(value) || '*E-mail inválido'
+                }
+            }
         }
-    }
+
+    },
+    methods: {
+        async cadastrar() {
+            
+            const { valid } = await this.$refs.form.validate()
+            
+            if (!valid) {
+                
+                return
+            }
+
+            const cadastro = {
+                name: this.nome,
+                email: this.email,
+                contact: this.contato,
+                date_birth: this.dataNasimento,
+                cep: this.CEP,
+                street: this.logradouro,
+                number: this.numero,
+                neighborhood: this.bairro,
+                city: this.cidade,
+                province: this.estado,
+                complement: this.complemento
+            }
+            this.$refs.form.reset()
+
+            try {
+                await axios.post('http://localhost:3000/students', cadastro)
+                    .then(() => {
+                        alert("Cadastrado com sucesso")
+
+                    })
+                    .catch((error) => {
+                        alert(error)
+                    })
+            } catch (error) {
+                console.log(error)
+            }
+
+
+        },
+        teste(){
+            console.log("Cheguei aqui")
+        }
+    },
 }
 </script>
